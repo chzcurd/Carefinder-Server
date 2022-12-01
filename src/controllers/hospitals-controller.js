ce2bc3d77a4bb5efc2f3ae79e9d88952c0086fce
Josh <darkernight0@gmail.com>
2022-11-30 21:58:13 -0600

    implemented search by distance

--- src/controllers/hospitals-controller.js
+++ src/controllers/hospitals-controller.js
@@ -15,11 +15,16 @@ hospitals-controller.js
  */
 exports.index = async (req, res) => {
   let searchObj = {};
-
   const params = Object.keys(req.query);
-
   const badVals = [];
 
+  //variables for distance searching
+  let rawdist = null;
+  let rawLat = null;
+  let rawLong = null;
+  let usingDist = false;
+
+  //Add params for all of the querys
   params.forEach((param) => {
     switch (param) {
       case "id":
@@ -57,9 +62,16 @@ exports.index = async (req, res) => {
         break;
       case "latitude":
         searchObj.latitude = regexWrap(req.query.latitude);
+        rawLat = req.query.latitude;
+
         break;
       case "longitude":
         searchObj.longitude = regexWrap(req.query.longitude);
+        rawLong = req.query.longitude;
+        break;
+      case "dist":
+        usingDist = true;
+        rawdist = req.query.dist;
         break;
       default:
         //passed in an invalid param
@@ -72,6 +84,56 @@ exports.index = async (req, res) => {
     return;
   }
 
+  //code to calc distance
+  if (usingDist) {
+    //parse values to numbers
+    const dist = parseFloat(rawdist);
+    const lat = parseFloat(rawLat);
+    const long = parseFloat(rawLong);
+
+    //check if dist is a number and is > 0
+    if (isNaN(dist) || dist <= 0) {
+      res
+        .status(400)
+        .send(
+          "dist needs to be a number > 0 (distance is in miles). Set as: " +
+            rawdist
+        );
+      return;
+    }
+    //error if lat and long arn't set
+    else if (isNaN(lat) || isNaN(long)) {
+      res
+        .status(400)
+        .send(
+          "latitude and longitude need to be set, or cannot be parsed to a number. Set as long: " +
+            rawLong +
+            " Lat: " +
+            rawLat
+        );
+      return;
+    } else {
+      //miles to meters conversion
+      const METERS_PER_MILE = 1609.34;
+
+      //add search method for location
+      searchObj.loc = {
+        $nearSphere: {
+          $geometry: {
+            type: "Point",
+            coordinates: [long, lat],
+          },
+          $maxDistance: dist * METERS_PER_MILE, //convert meters into miles, so we can search distance in miles
+        },
+      };
+
+      //remove lat and long from searchobj
+      delete searchObj.latitude;
+      delete searchObj.longitude;
+    }
+  }
+
+  //console log what came back
   console.log(searchObj);
   //Query the database
   const hospitals = await Hospital.find(searchObj).exec();
