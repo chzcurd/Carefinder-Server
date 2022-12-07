fc95a7864531edfc3d4aaaf296377d4fa6fbd65e
Josh <darkernight0@gmail.com>
2022-12-07 11:14:30 -0600

    verify that the user is an admin if the token says they are

--- src/middleware/jwt-handler.js
+++ src/middleware/jwt-handler.js
@@ -1,13 +1,14 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
 
 //verifies a JWT and adds username and isAdmin to req object
 exports.verifyJWT = (req, res, next) => {
   const token = req.headers.jwt;
   if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(400).json({ error: "jwt is required" });
   }
 
-  jwt.verify(token, process.env.jwtSecret, (err, decodedToken) => {
+  jwt.verify(token, process.env.jwtSecret, async (err, decodedToken) => {
     if (err) {
       console.log(err);
       return res.status(401).json({ error: "Unauthorized" });
@@ -18,8 +19,20 @@ exports.verifyJWT = (req, res, next) => {
 
     //set if admin is
     if (decodedToken.isAdmin === true) {
-      console.log("added admin");
-      req.isAdmin = true;
+      try {
+        //Check if user is still an admin
+        const user = await User.findOne({ username: decodedToken.username });
+        //user doesn't exist or is unauthorized
+        if (!user || user.isAdmin !== true) {
+          return res.status(401).json({ error: "Unauthorized" });
+        }
+
+        //they are still, add in that they are verified
+        req.isAdmin = true;
+      } catch (error) {
+        //catch any errors that come up
+        return res.status(401).json({ error: "Unauthorized" });
+      }
     }
     next();
   });
